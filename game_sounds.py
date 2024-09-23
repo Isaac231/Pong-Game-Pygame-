0db8dcc94f837051455407aec823bfec1d5b26a2
Isaac <juliusbalmaceda261@gmail.com>
2024-09-23 18:29:55 +0800

    FIXED SOUND BUGS AND ADDED COMMENTS

--- game_sounds.py
+++ game_sounds.py
@@ -3,24 +3,31 @@ import os
 
 pygame.mixer.init()
 
+# -----------SOUND SETUP----------
 sounds_dir = os.path.join(os.path.dirname(__file__), 'sounds')
 
 ball_hit_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "retro_blip.wav"))
 ball_out_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "game_over_sfx.wav"))
 wall_hit_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "wall_hit_sfx.wav"))
 score_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "retro-coin.wav"))
-counting_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "countdown_sound.wav"))
-start_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "start_btn_sfx.wav"))
 pause_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "pause_sfx.wav"))
+start_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "start_btn_sfx.wav"))
 final_score_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, "end_sfx.wav"))
 game_over = pygame.mixer.Sound(os.path.join(sounds_dir, "game_over_sfx2.wav"))
 intro_bgm = pygame.mixer.Sound(os.path.join(sounds_dir, "intro_bgm.wav"))
 ingame_bgm = os.path.join(os.path.dirname(__file__), 'sounds', 'ingame_bgm.wav')
+counting_sfx = pygame.mixer.Sound(os.path.join(sounds_dir, 'countdown_sound.wav'))
+
 pygame.mixer.music.load(ingame_bgm)
+
+countdown_chn = pygame.mixer.Channel(1)
+end_chn = pygame.mixer.Channel(2)
+out_chn = pygame.mixer.Channel(3)
+
 pygame.mixer.music.set_volume(0.3)
 
 
-# main bgm setup
+# ---------MAIN BGM SETUP--------------
 def play_main_bgm(loop=-1):
     pygame.mixer.music.play(loop)
 
@@ -38,51 +45,83 @@ def restart_main_bgm():
     pygame.mixer.music.play()
 
 
+# ---------COUNTDOWN SETUP--------------
+def play_count_sfx():
+    countdown_chn.play(counting_sfx)
+
+
+def pause_count_sfx():
+    countdown_chn.pause()
+
+
+def resume_count_sfx():
+    countdown_chn.unpause()
+
+
+# ---------MAX SCORE SFX SETUP--------------
+def play_end_sfx():
+    end_chn.play(final_score_sfx)
+
+
+def pause_end_sfx():
+    end_chn.pause()
+
+
+def resume_end_sfx():
+    end_chn.unpause()
+
+
+# ---------BALL OUT SFX SETUP--------------
+def play_ball_out():
+    out_chn.play(ball_out_sfx)
+
+
+def pause_ball_out():
+    out_chn.pause()
+
+
+def resume_ball_out():
+    out_chn.unpause()
+
+
+# ---------START BUTTON SFX SETUP--------------
+def play_start_sfx():
+    start_sfx.play()
+
+
 # sfx of ball hitting the paddles
 def play_ball_impact():
     ball_hit_sfx.play()
 
 
-# sfx of ball out of bounce
-def play_ball_out():
-    ball_out_sfx.set_volume(1.0)
-    ball_out_sfx.play()
-
-
-# sfx of ball hitting the edges of the screen
+# SFX WHEN HITTING THE EDGES OF THE WINDOW
 def play_hits_wall():
     wall_hit_sfx.play()
 
 
+# SFX WHEN SCORING
 def play_score_sfx():
     score_sfx.play()
 
 
-def play_count_sfx():
-    counting_sfx.play()
-
-
-def play_start_sfx():
-    start_sfx.play()
-
-
+# PAUSE SFX
 def play_pause_sfx():
     pause_sfx.play()
 
 
-def play_end_sfx():
-    final_score_sfx.play()
+# ---------GAME OVER SFX SETUP--------------
+def play_game_over_sfx(status=1):
+    if status == 1:
+        game_over.play()
+    else:
+        game_over.stop()
 
 
-def play_game_over_sfx():
-    game_over.play()
-
-
-# intro bgm setup
+# ---------INTRO BGM SETUP--------------
 def play_intro_bgm(status=1):
     loop = -1 if status == 1 else 0
     intro_bgm.set_volume(0.3)
     if status == 1:
         intro_bgm.play(loop)
-    elif status == 2:
+    else:
         intro_bgm.stop()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+#YES I KNOW MY CODE IS MESSY, SPECIALLY THE HARDCODED X, Y ARGUMENTS AND SUCH. TOO LAZY TO FIX THEM NOW HIHI
+
 import pygame
 from config import *
 import sys
 from game_sounds import play_ball_impact, play_ball_out, play_intro_bgm, play_main_bgm, pause_main_bgm, resume_main_bgm, \
-    restart_main_bgm, play_score_sfx, play_count_sfx, play_pause_sfx, play_start_sfx, play_end_sfx, play_game_over_sfx
+    restart_main_bgm, play_score_sfx, play_count_sfx, play_pause_sfx, play_start_sfx, play_end_sfx, play_game_over_sfx, \
+    resume_count_sfx, pause_count_sfx, pause_ball_out, pause_end_sfx, resume_end_sfx, resume_ball_out
 
 from sprites import Player, Ball, Enemy
 
@@ -11,11 +14,12 @@ pygame.init()
 
 class Game:
     def __init__(self):
-
+        # ----------SCREEN/FPS SETUP----------
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         pygame.display.set_caption("Pong Game")
         self.frame_rate = pygame.time.Clock()
 
+        # ----------FONT SETUP----------
         self.font_face1 = 'font/Blox2.ttf'
         self.font_face2 = 'font/joystix monospace.otf'
 
@@ -25,6 +29,7 @@ class Game:
         self.font4 = pygame.font.Font(self.font_face2, 15)
         self.font5 = pygame.font.Font(self.font_face2, 25)
 
+        # ----------BOOLEANS----------
         self.running = False
         self.sound_played = False
         self.count_played = False
@@ -37,6 +42,8 @@ class Game:
         self.is_game_over = False
         self.ball_collided = False
         self.winner_txt = None
+
+        # ----------COUNTERS----------
         self.collision_counter = 0
         self.counter1 = 0
         self.counter2 = 0
@@ -46,51 +53,76 @@ class Game:
         self.font_size = 15
         self.player_score = 0
         self.enemy_score = 0
-        self.max_score = 10
+        self.max_score = 2
 
+        # ----------SPRITES SETUP----------
         self.all_sprite = pygame.sprite.Group()
         self.all_player = pygame.sprite.GroupSingle()
         self.all_enemy = pygame.sprite.GroupSingle()
         self.all_ball = pygame.sprite.GroupSingle()
         self.all_intro_ball = pygame.sprite.GroupSingle()
 
+        # CREATE INSTANCES
         self.player = Player(self, 64, 176, )
-
         self.ball = Ball(self)
         self.intro_ball = Ball(self, 'intro')
-
         self.enemy = Enemy(self, 544, 176)
 
+        # ADD TO SPRITES GROUP
         self.all_sprite.add(self.player, self.ball, self.enemy)
         self.all_player.add(self.player)
         self.all_ball.add(self.ball)
         self.all_intro_ball.add(self.intro_ball)
         self.all_enemy.add(self.enemy)
 
-    def event(self):  # game loop event function
-        for event in pygame.event.get():  # checks if quit button is pressed
+    # -----------EVENTS SETUP----------
+    def event(self):
+        # CHECKS IF THE PLAYER PRESSED THE EXIT ON THE WINDOW
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
                 self.is_intro = False
 
-            if self.is_intro:  # starts the intro button blinking effect
+            # TRIGGERS BLINK EFFECT OF THE START BUTTON IN THE INTRO SCREEN
+            if self.is_intro:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.is_start_pressed = True
                         self.counter2 = 0
 
-            if event.type == pygame.KEYDOWN and not self.is_game_over:  # pauses the game
+            # PAUSES THE GAME AND SOME OTHER SOUNDS
+            if event.type == pygame.KEYDOWN and not self.is_game_over:
                 if event.key == pygame.K_SPACE:
                     self.is_paused = not self.is_paused
                     if self.is_paused:
                         play_pause_sfx()
                         pause_main_bgm()
-                    elif not self.is_paused:
-                        resume_main_bgm()
 
-    def update(self):  # game loop draw function
+                        if self.ball.is_out:
+                            pause_count_sfx()
+
+                            if self.player_score != self.max_score and self.enemy_score != self.max_score:
+                                pause_ball_out()
+                            else:
+                                pause_end_sfx()
+
+                    elif not self.is_paused:
+                        if not self.ball.is_out:
+                            resume_main_bgm()
+                        else:
+                            resume_count_sfx()
+
+                            if self.player_score != self.max_score and self.enemy_score != self.max_score:
+                                resume_ball_out()
+                            else:
+                                resume_end_sfx()
+
+    # -----------UPDATE SPRITES AND OTHERS----------
+    def update(self):
+        # UPDATE ALL SPRITES
         self.all_sprite.update()
 
+        # COUNTER OF COUNTDOWN SEQUENCE
         if self.ball.is_out and not self.is_paused:  # ball timer when out of bounce
             if not self.count_played:
                 pause_main_bgm()
@@ -108,17 +140,22 @@ class Game:
                     self.ball.is_out = False
                     self.sound_played = False
 
+        # CHECKS FOR COLLISIONS IF NOT PAUSED
         if not self.is_paused:
             self.if_collided()
 
+    # -----------DRAW SPRITES, TEXT AND OTHERS----------
     def draw(self):  # game loop draw function
+        # DRAW SCREEN
         self.screen.fill(black)
 
+        # DRAW A LINE IN THE MIDDLE OF THE WINDOW
         divider_line = pygame.Surface((5, 480))
         divider_line.fill(white)
         divider_rect = divider_line.get_rect(midtop=(320, 0))
         self.screen.blit(divider_line, divider_rect)
 
+        # INCREMENTS SCORE AND DISPLAYS WINNER AND COUNTDOWN TEXT
         if self.ball.is_out:
             if self.player_score != self.max_score and self.enemy_score != self.max_score:
                 if self.ball.ball_direction == 'right' and not self.scored:
@@ -145,31 +182,41 @@ class Game:
                 self.screen.blit(self.winner_txt, (320 - (tile_size * 8) + 5, 176 - (tile_size + 10)))
                 self.screen.blit(countdown_txt, (320 - tile_size, 176 + tile_size))
 
+        # DRAW ALL SPRITES
         self.all_sprite.draw(self.screen)
 
+        # DRAW PAUSE TEXT WHEN PAUSED
         self.game_paused()
+
+        # DRAW SCORE BOARD TEXT
         self.score_board()
 
+        # REDRAW THE WHOLE SCREEN
         pygame.display.update()
 
-    def if_collided(self):  # collision detection function
+    # -----------COLLISION SETUP----------
+    def if_collided(self):
 
-        def check_diagonally(ball, paddle):  # checks if the ball hits the top/bottom corner for diagonal movement
-            # create a three section for the paddle (top, center, and bottom)
+        # CHECKS IF THE BALL HITS THE TOP/BOTTOM PART OF THE PADDLE IF TRUE BALL MOVES DIAGONALLY
+        def check_diagonally(ball, paddle):
+            # CREATE THREE SECTION FOR THE PADDLE (TOP, CENTER, BOTTOM)
             paddle_top = pygame.Rect(paddle.rect.x, paddle.rect.y, tile_size, tile_size)
             paddle_center = pygame.Rect(paddle.rect.x, paddle.rect.y + tile_size, tile_size, tile_size)
             paddle_bottom = pygame.Rect(paddle.rect.x, paddle.rect.y + tile_size * 2, tile_size, tile_size)
 
-            # get the clipped rect
+            # USE CLIP METHOD TO CHECKS OVERLAYS
             top = paddle_top.clip(ball.rect)
             center = paddle_center.clip(ball.rect)
             bottom = paddle_bottom.clip(ball.rect)
 
-            # calculate the area of the clipped rect
+            # CALCULATE THE AREA OF THE CLIPPED RECT
             top_area = top.width * top.height
             center_area = center.width * center.height
             bottom_area = bottom.width * bottom.height
 
+            # COMPARE BOTH CLIPPED RECT TO SEE WHICH IS BIGGER
+            # THIS IS SO THAT IF THE BALL HITS TWO SECTION OF THE PADDLE
+            # IT WILL CHECK WHICH SECTION HAS THE LARGER OVERLAY AND PICKS IT
             if top_area > center_area and top_area > bottom_area:
                 return 'up'
             elif bottom_area > center_area and bottom_area > top_area:
@@ -177,16 +224,18 @@ class Game:
             else:
                 return ''
 
-        if pygame.sprite.spritecollide(self.ball, self.all_player, False):  # checks if collided for player
+        #CHECKS IF BALL COLLIDES WITH PLAYER PADDLE
+        if pygame.sprite.spritecollide(self.ball, self.all_player, False):
+            # ADDS COLLISION COUNTER TO PREVENT MULTIPLE EXECUTION
             if self.collision_counter == 0:
                 self.ball_collided = True
-                play_ball_impact()  # play ball collide sfx
+                play_ball_impact() #PLAY SFX
 
-                self.ball.ball_vertical = check_diagonally(self.ball, self.player)  # checks if it will move diagonally
+                self.ball.ball_vertical = check_diagonally(self.ball, self.player)  # CHECK IF DIAGONAL
 
                 self.ball.ball_direction = 'right'
                 if 0 <= self.ball.ball_speedup <= 25:
-                    self.ball.ball_speedup += 0.5  # gradually increase ball speed everytime it collides with paddles
+                    self.ball.ball_speedup += 0.5  # GRADUALLY INCREASE THE BALL SPEED
 
                 self.collision_counter = 1
         else:
@@ -199,6 +248,7 @@ class Game:
             if self.collision_counter > 0 and self.collision_counter >= 30:
                 self.collision_counter = 0
 
+        #SAME SETUP FOR ENEMY PADDLE
         if pygame.sprite.spritecollide(self.ball, self.all_enemy, False):  # checks if collided for enemy
             if self.collision_counter == 0:
                 self.ball_collided = True
@@ -222,17 +272,20 @@ class Game:
             if self.collision_counter > 0 and self.collision_counter >= 30:
                 self.collision_counter = 0
 
-    def score_board(self):  # game's scoring system
+    # -----------SCORE BOARD SETUP----------
+    def score_board(self):
         player_scoreboard = self.font2.render(f"{self.player_score}", True, white)
         self.screen.blit(player_scoreboard, (tile_size + (tile_size * 4), tile_size))
 
         enemy_scoreboard = self.font2.render(f"{self.enemy_score}", True, white)
         self.screen.blit(enemy_scoreboard, (screen_width - (tile_size * 6), tile_size))
 
-    def intro_update(self):  # intro screen update function
+
+    # -----------INTRO SCREEN SETUP----------
+    def intro_update(self):
         self.all_intro_ball.update()
 
-    def intro_draw(self):  # intro screen draw function
+    def intro_draw(self):
         self.screen.fill(black)
 
         if self.is_intro:
@@ -253,6 +306,7 @@ class Game:
         self.all_intro_ball.draw(self.screen)
         pygame.display.update()
 
+    #ALL COUNTER GETTER
     def get_frame_counts(self, section):
         color = white
         if section == 1:
@@ -295,6 +349,7 @@ class Game:
 
         return color
 
+    #ALL COUNTER CHECKER
     def frame_counters(self, section):
         if section == 1:  # intro_draw function
             if not self.is_start_pressed:  # counter loops
@@ -332,7 +387,7 @@ class Game:
             self.intro_draw()
             self.frame_rate.tick(FPS)
 
-    def game_paused(self):  # adds a paused indicator on top when paused
+    def game_paused(self):
         if self.is_paused:
             paused_txt = self.font4.render("PAUSED", True, self.get_frame_counts(3), pygame.SRCALPHA)
             self.screen.blit(paused_txt, (320 - tile_size, 176 - tile_size * 5))
@@ -343,14 +398,17 @@ class Game:
             self.counter2 = 0
             self.counter3 = 0
 
+    # -----------BUTTON SETUP----------
     def yes_button(self, text, x, y, width, height):
-        mouse_pos = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
+        mouse_pos = pygame.mouse.get_pos() #GET CURRENT MOUSE POSITION
+        click = pygame.mouse.get_pressed() #CHECK IF MOUSE CLICKS
 
+        #DRAW INVISIBLE RECT
         button_surface = pygame.Surface((width, height), pygame.SRCALPHA)
         button_rect = pygame.Rect(x, y, width, height)
         pygame.draw.rect(button_surface, transparent, button_surface.get_rect())
 
+        #CHECKS IF MOUSE LEFT CLICK AND HOVER EFFECT
         if button_rect.collidepoint(mouse_pos):
             self.font_size = 30
             if click[0] == 1:
@@ -362,11 +420,15 @@ class Game:
         font = pygame.font.Font(self.font_face2, self.font_size)
 
         yes_txt = font.render(text, True, white)
+
+        #DRAWS THE TEXT IN THE CENTER OF THE INVISIBLE RECT
         self.screen.blit(yes_txt,
                          (x + (width // 2 - yes_txt.get_width() // 2), y + (height // 2 - yes_txt.get_height() // 2)))
 
+        #RETURN FALSE IF NO LEFT CLICKS
         return False
 
+    #SAME SETUP
     def no_button(self, text, x, y, width, height):
         mouse_pos = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
@@ -391,6 +453,7 @@ class Game:
 
         return False
 
+    # -----------GAME OVER SETUP----------
     def game_over(self):
         self.screen.fill(black)
 
@@ -398,6 +461,7 @@ class Game:
             play_game_over_sfx()
             self.game_over_played = True
 
+        # -----------TEXT SETUP----------
         self.ball.winner = "Player 1" if self.player_score == self.max_score else "Player 2"
         game_over_txt = self.font3.render("Game Over", True, self.get_frame_counts(1))
         self.winner_txt = self.font5.render(f"{self.ball.winner} won the game!", True, white)
@@ -409,6 +473,7 @@ class Game:
         self.screen.blit(final_score_txt, (320 - tile_size * 2, 176 - tile_size))
         self.screen.blit(replay_txt, (320 - tile_size * 3.5, 176 + tile_size * 2))
 
+        # -----------CHECK FOR BUTTONS SETUP----------
         if self.yes_button("Yes", 320 - tile_size * 1.5, 176 + tile_size * 3, 80, 45):
             self.player_score, self.enemy_score = 0, 0
             self.ball.ball_speedup = 0
@@ -416,6 +481,7 @@ class Game:
             self.enemy.rect.topleft = self.enemy.default_pos
             self.is_game_over = False
             self.game_over_played = False
+            play_game_over_sfx(2)
             play_score_sfx()
             restart_main_bgm()
 
@@ -427,7 +493,8 @@ class Game:
         self.frame_counters(1)
         pygame.display.update()
 
-    def main(self):  # game loop runner
+    #MAIN LOOP RUNNER
+    def main(self):
         if self.running and not self.is_paused and not self.is_game_over:
             if not self.bgm_playing:
                 play_main_bgm()
@@ -438,7 +505,7 @@ class Game:
             self.draw()
         self.frame_rate.tick(FPS)
 
-
+#RUNNER
 if __name__ == '__main__':
     run = Game()
     run.intro_screen()
